1409d736bf63b9231c7a393e2752cfbd8b9d906c
Fumitaka Yamada <andpdas@gmail.com>
2015-01-30 19:28:08 +0900

    Initial commit

---
+++ 2048Game/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  2048Game
+//
+
+//  Copyright (c) 2015 Fumitaka Yamada. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ 2048Game/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  2048Game
+//
+
+//  Copyright (c) 2015 Fumitaka Yamada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
